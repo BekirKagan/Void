54e70096d259d10171dfc0be789238fecf0a0a18
BekirKagan <kagankaraahmetoglu@hotmail.com>
2024-08-06 13:09:45 +0300

    Project setup.

---
+++ lib/firebase.ts
@@ -0,0 +1,16 @@
+import "dotenv/config"
+import { initializeApp } from "firebase/app"
+
+const API_KEY = process.env.FIREBASE_API_KEY
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: "void-50ef4.firebaseapp.com",
+  projectId: "void-50ef4",
+  storageBucket: "void-50ef4.appspot.com",
+  messagingSenderId: "781848541289",
+  appId: "1:781848541289:web:634f06cc0c46abd4a50af8"
+}
+
+export const firebaseApp = initializeApp(firebaseConfig)
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,37 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'blue-chill': {
+          '50': '#f2f9f9',
+          '100': '#ddeff0',
+          '200': '#bfe0e2',
+          '300': '#92cace',
+          '400': '#5faab1',
+          '500': '#438e96',
+          '600': '#3b757f',
+          '700': '#356169',
+          '800': '#325158',
+          '900': '#2d464c',
+          '950': '#1a2c32',
+        },
+
+        'amber': {
+          '50': '#ffffea',
+          '100': '#fffbc5',
+          '200': '#fff885',
+          '300': '#ffed46',
+          '400': '#ffdf1b',
+          '500': '#ffbf00',
+          '600': '#e29300',
+          '700': '#bb6802',
+          '800': '#985008',
+          '900': '#7c420b',
+          '950': '#482200',
+        },
       },
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
